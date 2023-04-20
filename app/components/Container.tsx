5b4ea045d609cd103c531571e63c9376601631d2
formidabilus <chiriac_razvan@ymail.com>
2023-04-20 20:31:47 +0300

    added Logo and logo.png in images and created Search component

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+"use client";
 
-type Props = {};
+type ContainerProps = {
+  children: React.ReactNode;
+};
 
-const Container = (props: Props) => {
-  return <div>Container</div>;
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+      {children}
+    </div>
+  );
 };
 
 export default Container;
