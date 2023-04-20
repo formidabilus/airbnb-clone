0517e0f8c7a3752bd80c4f8abaa16a76ab7ca4a6
formidabilus <chiriac_razvan@ymail.com>
2023-04-20 19:42:59 +0300

    cleanup + added Navbar and Container components

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import Container from "../Container";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container></Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
