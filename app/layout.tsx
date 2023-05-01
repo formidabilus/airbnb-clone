da6f572ebf893de99d42649204b8dd5fba17b8de
formidabilus <chiriac_razvan@ymail.com>
2023-05-01 17:27:37 +0300

    created useLoginModal hook in hooks/ and LoginModal component in modals/

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import { Nunito } from "next/font/google";
 
 import "./globals.css";
+import ToasterProvider from "./providers/ToasterProvider";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
-import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
 
 export const metadata = {
   title: "Airbnb",
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
