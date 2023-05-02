76f4fc624d00e853343fd547de24e11ee6602b57
formidabilus <chiriac_razvan@ymail.com>
2023-05-02 18:24:58 +0300

    added more categories in Categories component

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -43,10 +43,11 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
       { skipNull: true }
     );
     router.push(url);
-  }, []);
+  }, [label, params, router]);
 
   return (
     <div
+      onClick={handleClick}
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer 
     ${selected ? "border-b-neutral-800" : "border-transparent"}
     ${selected ? "text-neutral-800" : "text-neutral-500"} 
