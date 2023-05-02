d77c353ca8cbe65637183afb8cd6998b58bf728c
formidabilus <chiriac_razvan@ymail.com>
2023-05-02 19:00:51 +0300

    added RentModal component in modals and useRentModal hook in hooks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import { Nunito } from "next/font/google";
 
 import "./globals.css";
-import ToasterProvider from "./providers/ToasterProvider";
 import Navbar from "./components/navbar/Navbar";
-import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import LoginModal from "./components/modals/LoginModal";
+import RentModal from "./components/modals/RentModal";
+import ClientOnly from "./components/ClientOnly";
+import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
@@ -29,6 +30,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <RentModal />
           <LoginModal />
           <RegisterModal />
           <Navbar currentUser={currentUser} />
