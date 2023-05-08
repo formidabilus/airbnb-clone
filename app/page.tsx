f1214eabcda010607eac2538148b6ca5133d1606
formidabilus <chiriac_razvan@ymail.com>
2023-05-08 23:19:00 +0300

    created listings folder in components and added there an empty ListingCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
@@ -20,7 +24,9 @@ export default function Home() {
           className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6
          gap-8"
         >
-          <div>My future listings</div>
+          {listings.map((listing: any) => {
+            return <ListingCard key={listing.id} data={listing} />;
+          })}
         </div>
       </Container>
     </ClientOnly>
