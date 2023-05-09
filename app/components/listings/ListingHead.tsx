d36cab70780eb6af9d2cc09e5eabfd192dda0ea7
formidabilus <chiriac_razvan@ymail.com>
2023-05-10 02:43:16 +0300

    added interface for props type in ListingHead component

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,6 +1,22 @@
 "use client";
 
-const ListingHead = () => {
+import { SafeUser } from "@/app/types";
+
+interface ListingHeadProps {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({
+  title,
+  locationValue,
+  imageSrc,
+  id,
+  currentUser,
+}) => {
   return <div>ListingHead</div>;
 };
 
