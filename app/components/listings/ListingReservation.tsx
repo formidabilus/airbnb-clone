1a15093ee2526e01f02cd689759a1ec43c9a2c3c
formidabilus <chiriac_razvan@ymail.com>
2023-05-15 16:18:38 +0300

    started styling ListingReservation, created Calendar component in inputs/ and added it to ListingReservation

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,5 +1,41 @@
-const ListingReservation = () => {
-  return <div></div>;
+"use client";
+
+import { Range } from "react-date-range";
+import Calendar from "../inputs/Calendar";
+
+interface ListingReservationProps {
+  price: number;
+  dateRange: Range;
+  totalPrince: number;
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+  disabled?: boolean;
+  disabledDates: Date[];
+}
+
+const ListingReservation: React.FC<ListingReservationProps> = ({
+  price,
+  dateRange,
+  totalPrince,
+  onChangeDate,
+  onSubmit,
+  disabled,
+  disabledDates,
+}) => {
+  return (
+    <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold"> $ {price}</div>
+        <div className="font-light text-neutral-600">night</div>
+      </div>
+      <hr />
+      <Calendar
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value) => onChangeDate(value.selection)}
+      />
+    </div>
+  );
 };
 
 export default ListingReservation;
