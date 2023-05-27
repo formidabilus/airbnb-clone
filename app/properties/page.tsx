bd45ea81e9668b4fc8f50b6f199442a5c7ead061
formidabilus <chiriac_razvan@ymail.com>
2023-05-27 17:34:26 +0300

    added IListingsParams interface and exported it and added where query for listings in getListings action, changed Home page to take IListingParams in getListings as searchParams, copied and renamed trips folder to properties folder and started to work on it

---
+++ app/properties/page.tsx
@@ -0,0 +1,38 @@
+import EmptyState from "../components/EmptyState";
+import ClientsOnly from "../components/ClientOnly";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+import TripsClient from "./TripsClient";
+
+const PropertiesPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientsOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientsOnly>
+    );
+  }
+  const reservations = await getReservations({ userId: currentUser.id });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientsOnly>
+        <EmptyState
+          title="No trips found"
+          subtitle="Looks like you haven't reserved any trips"
+        />
+      </ClientsOnly>
+    );
+  }
+
+  return (
+    <ClientsOnly>
+      <TripsClient reservations={reservations} currentUser={currentUser} />
+    </ClientsOnly>
+  );
+};
+
+export default PropertiesPage;
