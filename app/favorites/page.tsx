f84c1a5247a72d117baa5753893fc55c755d2cf2
formidabilus <chiriac_razvan@ymail.com>
2023-05-27 16:55:22 +0300

    created getFavoriteListings action in actions/ and imported it to ListingPage in favorites/

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -2,6 +2,7 @@ import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
 
 import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
 
 const ListingPage = async () => {
   return (
