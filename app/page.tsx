4e3b8a3ffa1a7951c4fe4cf9f6bb22047aaa69a7
formidabilus <chiriac_razvan@ymail.com>
2023-06-05 18:20:25 +0300

    added force-dynamic to page.tsx to solve the deployment error

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ interface HomeProps {
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
+
   if (listings.length === 0) {
     return (
       <ClientOnly>
@@ -27,7 +28,7 @@ const Home = async ({ searchParams }: HomeProps) => {
           className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6
          gap-8"
         >
-          {listings.map((listing) => {
+          {listings.map((listing: any) => {
             return (
               <ListingCard
                 currentUser={currentUser}
