9d97f9189bc711b7b80ae4d3075fc6db3ce73d4c
formidabilus <chiriac_razvan@ymail.com>
2023-07-12 00:59:31 +0300

    trying to solve middleware server error: no secret

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,7 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+export default withAuth({
+  secret: process.env.SECRET,
+});
 
 export const config = {
   matcher: ["/trips", "/reservations", "/properties", "/favorites"],
